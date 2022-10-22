b6c5b3e4e3ee29ddc7b64566aae899ab69d43eb0
yoshoku <yoshoku@outlook.com>
2022-10-22 17:10:58 +0900

    chore: remove unnecessary error class

--- lib/numo/random.rb
+++ lib/numo/random.rb
@@ -4,10 +4,3 @@ require 'numo/narray'
 
 require_relative 'random/version'
 require_relative 'random/randomext'
-
-module Numo
-  module Random
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
