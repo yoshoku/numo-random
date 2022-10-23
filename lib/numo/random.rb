bb2b0e7e4a11d56992f28f4e57739eb80af3f066
yoshoku <yoshoku@outlook.com>
2022-10-23 13:39:12 +0900

    feat: add generator class

--- lib/numo/random.rb
+++ lib/numo/random.rb
@@ -4,3 +4,181 @@ require 'numo/narray'
 
 require_relative 'random/version'
 require_relative 'random/randomext'
+
+# Ruby/Numo (NUmerical MOdules)
+module Numo
+  # Numo::Random provides random number generation with several distributions for Numo::NArray.
+  module Random
+    # Generator is a class that generates random number with several distributions.
+    #
+    # @example
+    #   require 'numo/random'
+    #
+    #   x = Numo::DFloat.new(100)
+    #
+    #   rng = Numo::Random::Generator.new
+    #   rng.uniform(x, low: -1, high: 2)
+    class Generator
+      # Returns random number generation algorithm.
+      # @return [String]
+      attr_accessor :algorithm
+
+      # Creates a new random number generator.
+      #
+      # @param seed [Integer] random seed used to initialize the random number generator.
+      # @param algorithm [String] random number generation algorithm.
+      def initialize(seed: nil, algorithm: 'pcg64') # rubocop:disable Lint/UnusedMethodArgument
+        @algorithm = 'pcg64'
+        @rng = PCG64.new(seed: seed)
+      end
+
+      # Returns the seed of random number generator.
+      #
+      # @return [Integer]
+      def seed
+        rng.seed
+      end
+
+      # Sets the seed of random number generator.
+      #
+      # @param val [Integer] random seed.
+      def seed=(val)
+        rng.seed = val
+      end
+
+      # Returns random number with uniform distribution in the half-open interval [0, 1).
+      #
+      # @example
+      #   require 'numo/random'
+      #
+      #   rng = Numo::Random::Generator.new
+      #   v = rng.random
+      #
+      # @return [Float]
+      def random
+        rng.random
+      end
+
+      # Fills given array with uniformly distributed random values in the interval [low, high).
+      #
+      # @example
+      #   require 'numo/random'
+      #
+      #   x = Numo::DFloat.new(100)
+      #
+      #   rng = Numo::Random::Generator.new
+      #   rng.uniform(x, low: -1.5, high: 1.5)
+      #
+      # @param x [Numo::DFloat | Numo::SFloat] array filled with random values.
+      # @param low [Float] lower boundary.
+      # @param high [Float] upper boundary.
+      def uniform(x, low: 0.0, high: 1.0)
+        rng.uniform(x, low: low, high: high)
+      end
+
+      # Fills given array with random values according to the Cauchy (Lorentz) distribution.
+      #
+      # @example
+      #   require 'numo/random'
+      #
+      #   x = Numo::DFloat.new(100)
+      #
+      #   rng = Numo::Random::Generator.new
+      #   rng.cauchy(x, loc: 0.0, scale: 1.0)
+      #
+      # @param x [Numo::DFloat | Numo::SFloat] array filled with random values.
+      # @param loc [Float] location parameter.
+      # @param scale [Float] scale parameter.
+      def cauchy(x, loc: 0.0, scale: 1.0)
+        rng.cauchy(x, loc: loc, scale: scale)
+      end
+
+      # Fills given array with random values according to the Chi-squared distribution.
+      #
+      # @example
+      #   require 'numo/random'
+      #
+      #   x = Numo::DFloat.new(100)
+      #
+      #   rng = Numo::Random::Generator.new
+      #   rng.chisquare(x, df: 2.0)
+      #
+      # @param x [Numo::DFloat | Numo::SFloat] array filled with random values.
+      # @param df [Float] degrees of freedom, must be > 0.
+      def chisquare(x, df:)
+        rng.chisquare(x, df: df)
+      end
+
+      # Fills given array with random values according to the F-distribution.
+      #
+      # @example
+      #   require 'numo/random'
+      #
+      #   x = Numo::DFloat.new(100)
+      #
+      #   rng = Numo::Random::Generator.new
+      #   rng.f(x, dfnum: 2.0, dfden: 4.0)
+      #
+      # @param x [Numo::DFloat | Numo::SFloat] array filled with random values.
+      # @param dfnum [Float] degrees of freedom in numerator, must be > 0.
+      # @param dfden [Float] degrees of freedom in denominator, must be > 0.
+      def f(x, dfnum:, dfden:)
+        rng.f(x, dfnum: dfnum, dfden: dfden)
+      end
+
+      # Fills given array with random values according to a normal (Gaussian) distribution.
+      #
+      # @example
+      #   require 'numo/random'
+      #
+      #   x = Numo::DFloat.new(100)
+      #
+      #   rng = Numo::Random::Generator.new
+      #   rng.normal(x, loc: 0.0, scale: 1.0)
+      #
+      # @param x [Numo::DFloat | Numo::SFloat] array filled with random values.
+      # @param loc [Float] location parameter.
+      # @param scale [Float] scale parameter.
+      def normal(x, loc: 0.0, scale: 1.0)
+        rng.normal(x, loc: loc, scale: scale)
+      end
+
+      # Fills given array with random values according to a log-normal distribution.
+      #
+      # @example
+      #   require 'numo/random'
+      #
+      #   x = Numo::DFloat.new(100)
+      #
+      #   rng = Numo::Random::Generator.new
+      #   rng.lognormal(x, mean: 0.0, sigma: 1.0)
+      #
+      # @param x [Numo::DFloat | Numo::SFloat] array filled with random values.
+      # @param mean [Float] mean of normal distribution.
+      # @param sigma [Float] standard deviation of normal distribution.
+      def lognormal(x, mean: 0.0, sigma: 1.0)
+        rng.lognormal(x, mean: mean, sigma: sigma)
+      end
+
+      # Fills given array with random values according to the Student's t-distribution.
+      #
+      # @example
+      #   require 'numo/random'
+      #
+      #   x = Numo::DFloat.new(100)
+      #
+      #   rng = Numo::Random::Generator.new
+      #   rng.standard_t(x, df: 8.0)
+      #
+      # @param x [Numo::DFloat | Numo::SFloat] array filled with random values.
+      # @param df [Float] degrees of freedom, must be > 0.
+      def standard_t(x, df:)
+        rng.standard_t(x, df: df)
+      end
+
+      private
+
+      attr_reader :rng
+    end
+  end
+end
---
+++ spec/numo/random/generator_spec.rb
@@ -0,0 +1,391 @@
+# frozen_string_literal: true
+
+RSpec.describe Numo::Random::Generator do
+  subject(:rng) { described_class.new(seed: 42) }
+
+  describe '#seed= and #seed' do
+    it 'sets and gets random seed', :aggregate_failures do
+      expect(rng.seed).to eq(42)
+      rng.seed = 100
+      expect(rng.seed).to eq(100)
+    end
+  end
+
+  describe '#random' do
+    it 'gets random number' do
+      expect(rng.random).not_to be_nil
+    end
+  end
+
+  describe '#uniform' do
+    context 'when array type is DFloat' do
+      let(:x) { Numo::DFloat.new(500, 600) }
+      let(:y) { rng.uniform(x, low: 1, high: 4) }
+
+      it 'obtains random numbers form a uniform distribution', :aggregate_failures do
+        expect(y).to be_a(Numo::DFloat)
+        expect(y.shape).to match(x.shape)
+        expect(y.mean).to be_within(1e-2).of(2.5)
+        expect(y.var).to be_within(1e-2).of(0.75)
+      end
+    end
+
+    context 'when array type is SFloat' do
+      let(:x) { Numo::SFloat.new(500, 600) }
+      let(:y) { rng.uniform(x, low: 1, high: 4) }
+
+      it 'obtains random numbers form a uniform distribution', :aggregate_failures do
+        expect(y).to be_a(Numo::SFloat)
+        expect(y.shape).to match(x.shape)
+        expect(y.mean).to be_within(1e-2).of(2.5)
+        expect(y.var).to be_within(1e-2).of(0.75)
+      end
+    end
+
+    context 'when high - low is negative value' do
+      let(:x) { Numo::DFloat.new(5, 2) }
+
+      it 'raises ArgumentError' do
+        expect { rng.uniform(x, low: 10, high: 5) }.to raise_error(ArgumentError, 'high - low must be > 0')
+      end
+    end
+
+    context 'when array type is Int32' do
+      let(:x) { Numo::Int32.new(5, 2) }
+
+      it 'raises TypeError' do
+        expect { rng.uniform(x) }.to raise_error(TypeError, 'invalid NArray class, it must be DFloat or SFloat')
+      end
+    end
+  end
+
+  describe '#cauchy' do
+    let(:mad) { (y - y.median).abs.median }
+
+    context 'when array type is DFloat' do
+      let(:x) { Numo::DFloat.new(500, 200) }
+      let(:y) { rng.cauchy(x) }
+
+      it 'obtains random numbers form a cauchy distribution', :aggregate_failures do
+        expect(y).to be_a(Numo::DFloat)
+        expect(y.shape).to match(x.shape)
+        expect(y.median).to be_within(1e-2).of(0)
+        expect(mad).to be_within(1e-2).of(1)
+      end
+    end
+
+    context 'when array type is SFloat' do
+      let(:x) { Numo::SFloat.new(500, 200) }
+      let(:y) { rng.cauchy(x) }
+
+      it 'obtains random numbers form a normal distribution', :aggregate_failures do
+        expect(y).to be_a(Numo::SFloat)
+        expect(y.shape).to match(x.shape)
+        expect(y.median).to be_within(1e-2).of(0)
+        expect(mad).to be_within(1e-2).of(1)
+      end
+    end
+
+    context 'when loc and scale parameters are given' do
+      let(:x) { Numo::DFloat.new(500, 200) }
+      let(:y) { rng.cauchy(x, loc: 4, scale: 2) }
+
+      it 'obtains random numbers form a normal distribution along with given parameters', :aggregate_failures do
+        expect(y.median).to be_within(1e-2).of(4)
+        expect(mad).to be_within(1e-2).of(2)
+      end
+    end
+
+    context 'when negative value is given to scale' do
+      let(:x) { Numo::DFloat.new(5, 2) }
+
+      it 'raises ArgumentError' do
+        expect { rng.cauchy(x, scale: -100) }.to raise_error(ArgumentError, 'scale must be a non-negative value')
+      end
+    end
+
+    context 'when array type is Int32' do
+      let(:x) { Numo::Int32.new(5, 2) }
+
+      it 'raises TypeError' do
+        expect { rng.cauchy(x) }.to raise_error(TypeError, 'invalid NArray class, it must be DFloat or SFloat')
+      end
+    end
+  end
+
+  describe '#chisquare' do
+    context 'when array type is DFloat' do
+      let(:x) { Numo::DFloat.new(500, 600) }
+      let(:y) { rng.chisquare(x, df: 2) }
+
+      it 'obtains random numbers form a chi-squared distribution', :aggregate_failures do
+        expect(y).to be_a(Numo::DFloat)
+        expect(y.shape).to match(x.shape)
+        expect(y.mean).to be_within(1e-2).of(2)
+        expect(y.var).to be_within(1e-1).of(4)
+      end
+    end
+
+    context 'when array type is SFloat' do
+      let(:x) { Numo::SFloat.new(500, 600) }
+      let(:y) { rng.chisquare(x, df: 2) }
+
+      it 'obtains random numbers form a chi-squared distribution', :aggregate_failures do
+        expect(y).to be_a(Numo::SFloat)
+        expect(y.shape).to match(x.shape)
+        expect(y.mean).to be_within(1e-2).of(2)
+        expect(y.var).to be_within(1e-1).of(4)
+      end
+    end
+
+    context 'when negative value is given to df' do
+      let(:x) { Numo::DFloat.new(5, 2) }
+
+      it 'raises ArgumentError' do
+        expect { rng.chisquare(x, df: -1) }.to raise_error(ArgumentError, 'df must be > 0')
+      end
+    end
+
+    context 'when zero is given to df' do
+      let(:x) { Numo::DFloat.new(5, 2) }
+
+      it 'raises ArgumentError' do
+        expect { rng.chisquare(x, df: 0) }.to raise_error(ArgumentError, 'df must be > 0')
+      end
+    end
+
+    context 'when array type is Int32' do
+      let(:x) { Numo::Int32.new(5, 2) }
+
+      it 'raises TypeError' do
+        expect do
+          rng.chisquare(x, df: 1)
+        end.to raise_error(TypeError, 'invalid NArray class, it must be DFloat or SFloat')
+      end
+    end
+  end
+
+  describe '#f' do
+    context 'when array type is DFloat' do
+      let(:x) { Numo::DFloat.new(500, 600) }
+      let(:y) { rng.f(x, dfnum: 5, dfden: 10) }
+
+      it 'obtains random numbers form a F-distribution', :aggregate_failures do
+        expect(y).to be_a(Numo::DFloat)
+        expect(y.shape).to match(x.shape)
+        expect(y.mean).to be_within(1e-2).of(1.25)
+        expect(y.var).to be_within(1e-1).of(1.354)
+      end
+    end
+
+    context 'when array type is SFloat' do
+      let(:x) { Numo::SFloat.new(500, 600) }
+      let(:y) { rng.f(x, dfnum: 5, dfden: 10) }
+
+      it 'obtains random numbers form a F-distribution', :aggregate_failures do
+        expect(y).to be_a(Numo::SFloat)
+        expect(y.shape).to match(x.shape)
+        expect(y.mean).to be_within(1e-2).of(1.25)
+        expect(y.var).to be_within(1e-1).of(1.354)
+      end
+    end
+
+    context 'when negative value is given to dfnum' do
+      let(:x) { Numo::DFloat.new(5, 2) }
+
+      it 'raises ArgumentError' do
+        expect { rng.f(x, dfnum: -5, dfden: 10) }.to raise_error(ArgumentError, 'dfnum must be > 0')
+      end
+    end
+
+    context 'when negative value is given to dfden' do
+      let(:x) { Numo::DFloat.new(5, 2) }
+
+      it 'raises ArgumentError' do
+        expect { rng.f(x, dfnum: 5, dfden: -10) }.to raise_error(ArgumentError, 'dfden must be > 0')
+      end
+    end
+
+    context 'when zero is given to dfnum' do
+      let(:x) { Numo::DFloat.new(5, 2) }
+
+      it 'raises ArgumentError' do
+        expect { rng.f(x, dfnum: 0, dfden: 10) }.to raise_error(ArgumentError, 'dfnum must be > 0')
+      end
+    end
+
+    context 'when zero is given to dfden' do
+      let(:x) { Numo::DFloat.new(5, 2) }
+
+      it 'raises ArgumentError' do
+        expect { rng.f(x, dfnum: 5, dfden: 0) }.to raise_error(ArgumentError, 'dfden must be > 0')
+      end
+    end
+
+    context 'when array type is Int32' do
+      let(:x) { Numo::Int32.new(5, 2) }
+
+      it 'raises TypeError' do
+        expect do
+          rng.f(x, dfnum: 5, dfden: 10)
+        end.to raise_error(TypeError, 'invalid NArray class, it must be DFloat or SFloat')
+      end
+    end
+  end
+
+  describe '#normal' do
+    context 'when array type is DFloat' do
+      let(:x) { Numo::DFloat.new(500, 200) }
+      let(:y) { rng.normal(x) }
+
+      it 'obtains random numbers form a normal distribution', :aggregate_failures do
+        expect(y).to be_a(Numo::DFloat)
+        expect(y.shape).to match(x.shape)
+        expect(y.mean).to be_within(1e-2).of(0)
+        expect(y.stddev).to be_within(1e-2).of(1)
+      end
+    end
+
+    context 'when array type is SFloat' do
+      let(:x) { Numo::SFloat.new(500, 200) }
+      let(:y) { rng.normal(x) }
+
+      it 'obtains random numbers form a normal distribution', :aggregate_failures do
+        expect(y).to be_a(Numo::SFloat)
+        expect(y.shape).to match(x.shape)
+        expect(y.mean).to be_within(1e-2).of(0)
+        expect(y.stddev).to be_within(1e-2).of(1)
+      end
+    end
+
+    context 'when loc and scale parameters are given' do
+      let(:x) { Numo::DFloat.new(500, 200) }
+      let(:y) { rng.normal(x, loc: 10, scale: 2) }
+
+      it 'obtains random numbers form a normal distribution along with given parameters', :aggregate_failures do
+        expect(y.mean).to be_within(1e-2).of(10)
+        expect(y.stddev).to be_within(1e-2).of(2)
+      end
+    end
+
+    context 'when negative value is given to scale' do
+      let(:x) { Numo::DFloat.new(500, 200) }
+
+      it 'raises ArgumentError' do
+        expect { rng.normal(x, scale: -100) }.to raise_error(ArgumentError, 'scale must be a non-negative value')
+      end
+    end
+
+    context 'when array type is Int32' do
+      let(:x) { Numo::Int32.new(500, 200) }
+
+      it 'raises TypeError' do
+        expect { rng.normal(x) }.to raise_error(TypeError, 'invalid NArray class, it must be DFloat or SFloat')
+      end
+    end
+  end
+
+  describe '#lognormal' do
+    context 'when array type is DFloat' do
+      let(:x) { Numo::DFloat.new(500, 600) }
+      let(:y) { rng.lognormal(x) }
+
+      it 'obtains random numbers form a lognormal distribution', :aggregate_failures do
+        expect(y).to be_a(Numo::DFloat)
+        expect(y.shape).to match(x.shape)
+        expect(y.mean).to be_within(1e-2).of(Math.exp(0.5))
+        expect(y.var).to be_within(1e-1).of(Math.exp(2) - Math.exp(1))
+      end
+    end
+
+    context 'when array type is SFloat' do
+      let(:x) { Numo::SFloat.new(500, 600) }
+      let(:y) { rng.lognormal(x) }
+
+      it 'obtains random numbers form a lognormal distribution', :aggregate_failures do
+        expect(y).to be_a(Numo::SFloat)
+        expect(y.shape).to match(x.shape)
+        expect(y.mean).to be_within(1e-2).of(Math.exp(0.5))
+        expect(y.var).to be_within(1e-1).of(Math.exp(2) - Math.exp(1))
+      end
+    end
+
+    context 'when loc and scale parameters are given' do
+      let(:x) { Numo::DFloat.new(500, 800) }
+      let(:y) { rng.lognormal(x, mean: 0.5, sigma: 1) }
+
+      it 'obtains random numbers form a lognormal distribution along with given parameters', :aggregate_failures do
+        expect(y.mean).to be_within(1e-2).of(Math.exp(1))
+        expect(y.var).to be_within(1e-1).of(Math.exp(3) - Math.exp(2))
+      end
+    end
+
+    context 'when negative value is given to sigma' do
+      let(:x) { Numo::DFloat.new(5, 2) }
+
+      it 'raises ArgumentError' do
+        expect { rng.lognormal(x, sigma: -100) }.to raise_error(ArgumentError, 'sigma must be a non-negative value')
+      end
+    end
+
+    context 'when array type is Int32' do
+      let(:x) { Numo::Int32.new(5, 2) }
+
+      it 'raises TypeError' do
+        expect { rng.lognormal(x) }.to raise_error(TypeError, 'invalid NArray class, it must be DFloat or SFloat')
+      end
+    end
+  end
+
+  describe '#standard_t' do
+    context 'when array type is DFloat' do
+      let(:x) { Numo::DFloat.new(500, 600) }
+      let(:y) { rng.standard_t(x, df: 10) }
+
+      it "obtains random numbers form a Student's t-distribution", :aggregate_failures do
+        expect(y).to be_a(Numo::DFloat)
+        expect(y.shape).to match(x.shape)
+        expect(y.mean).to be_within(1e-2).of(0)
+        expect(y.var).to be_within(1e-2).of(1.25)
+      end
+    end
+
+    context 'when array type is SFloat' do
+      let(:x) { Numo::SFloat.new(500, 600) }
+      let(:y) { rng.standard_t(x, df: 10) }
+
+      it "obtains random numbers form a Student's t-distribution", :aggregate_failures do
+        expect(y).to be_a(Numo::SFloat)
+        expect(y.shape).to match(x.shape)
+        expect(y.mean).to be_within(1e-2).of(0)
+        expect(y.var).to be_within(1e-2).of(1.25)
+      end
+    end
+
+    context 'when negative value is given to df' do
+      let(:x) { Numo::DFloat.new(5, 2) }
+
+      it 'raises ArgumentError' do
+        expect { rng.standard_t(x, df: -1) }.to raise_error(ArgumentError, 'df must be > 0')
+      end
+    end
+
+    context 'when zero is given to df' do
+      let(:x) { Numo::DFloat.new(5, 2) }
+
+      it 'raises ArgumentError' do
+        expect { rng.standard_t(x, df: 0) }.to raise_error(ArgumentError, 'df must be > 0')
+      end
+    end
+
+    context 'when array type is Int32' do
+      let(:x) { Numo::Int32.new(5, 2) }
+
+      it 'raises TypeError' do
+        expect do
+          rng.standard_t(x, df: 1)
+        end.to raise_error(TypeError, 'invalid NArray class, it must be DFloat or SFloat')
+      end
+    end
+  end
+end
