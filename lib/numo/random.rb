7df924100837e99e8042ba8aeee0d1d048b8b8ee
yoshoku <yoshoku@outlook.com>
2025-12-30 18:16:57 +0900

    docs: fix comment for require method to load native extension

--- lib/numo/random.rb
+++ lib/numo/random.rb
@@ -3,7 +3,7 @@
 require 'numo/narray/alt'
 
 require_relative 'random/version'
-# On distributions like Rocky Linux, native extensions are installed in a separate
+# On RHEL-based Linux distributions, native extensions are installed in a separate
 # directory from Ruby code, so use require to load them.
 require 'numo/random/ext'
 require_relative 'random/generator'
